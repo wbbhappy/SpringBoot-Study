3472634063a79cb56d1cc5a090ec5fb00ccd00ab
yihonglei <yihonglei@bcpeople.com>
2019-01-07 17:02:23 +0800

    scheduled任务

---
+++ springboot-base/springboot/src/main/java/com/lanhuigu/springboot/config/ScheduleConfig.java
@@ -0,0 +1,29 @@
+package com.lanhuigu.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * 修改Spring Scheculed默认线程池
+ *
+ * @author yihonglei
+ * @date 2019/1/7 15:47
+ */
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(new ScheduledThreadPoolExecutor(10,
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(Runnable r) {
+                        return new Thread(r, "my-schedule-pool");
+                    }
+                }));
+    }
+}
---
+++ springboot-base/springboot/src/main/java/com/lanhuigu/springboot/task/BootTask.java
@@ -0,0 +1,31 @@
+package com.lanhuigu.springboot.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yihonglei
+ * @date 2019/1/7 16:54
+ */
+@Component
+public class BootTask {
+
+    @Scheduled(fixedDelay = 1000)
+    public void test01() {
+        System.out.println("test01 start");
+        System.out.println("执行业务逻辑...");
+        System.out.println("test01 end");
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void test02() {
+        System.out.println("test02 start");
+        try {
+            Thread.sleep(60*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("test02 end");
+    }
+
+}
