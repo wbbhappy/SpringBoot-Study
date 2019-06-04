30fe1a04d8a117257c89e7a4f219ac75fb46d542
yihonglei <yihonglei@bcpeople.com>
2019-06-04 13:44:42 +0800

    thymeleaf模板引擎

---
+++ springboot-base/springboot/src/main/java/com/lanhuigu/springboot/controller/TemplatesController.java
@@ -0,0 +1,29 @@
+package com.lanhuigu.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * 注意:
+ * 这个地方使用的是@Controller，而不是@RestController，用的是spring MVC逻辑，
+ * 在spring MVC中，需要在spring mvc配置的视图解析器中指定视图文件位置，spring boot使用
+ * thymeleaf等于将视图地址默认在src/main/resources/templates下了
+ *
+ * @auther: yihonglei
+ * @date: 2019-06-04 13:32
+ */
+@Controller
+@RequestMapping("/templates")
+public class TemplatesController {
+
+    @RequestMapping("/helloHtml")
+    public String helloHtml(Map<String, Object> map) {
+        map.put("nameKey", "Thymeleaf");
+
+        // 返回的hello对应src/main/resources/templates下的hello.html页面
+        return "hello";
+    }
+
+}
