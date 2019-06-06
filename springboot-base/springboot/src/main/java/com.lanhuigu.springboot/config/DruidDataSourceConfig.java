4287487a8ff291910e13fcfe77ac469b4c94f516
yihonglei <yihonglei@bcpeople.com>
2019-06-06 15:25:35 +0800

    druid控制台

--- springboot-base/springboot/src/main/java/com.lanhuigu.springboot/config/DruidDataSourceConfig.java
+++ springboot-base/springboot/src/main/java/com.lanhuigu.springboot/config/DruidDataSourceConfig.java
@@ -1,14 +1,21 @@
 package com.lanhuigu.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 使用druid配置数据库属性
@@ -22,6 +29,9 @@ public class DruidDataSourceConfig {
     @Autowired
     private DruidDataSourceProperties druidDataSourceProperties;
 
+    /**
+     * 配置数据源
+     */
     @Bean("dataSource")
     @Primary
     public DataSource dataSource() throws SQLException {
@@ -38,5 +48,32 @@ public class DruidDataSourceConfig {
         druidDataSourceProperties.setPoolPreparedStatements(druidDataSourceProperties.isPoolPreparedStatements());
 
         return druidDataSource;
+    }
+
+    /**
+     * 配置druid管理后台的servlet
+     */
+    @Bean
+    public ServletRegistrationBean statViewSerlvet() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String, Object> initParameters = new HashMap<>();
+        initParameters.put("loginUsername", "admin");
+        initParameters.put("loginPassword", "123456");
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+
+    /**
+     * 配置防火墙
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+
+        Map<String, Object> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.css,/druid/*");
+        filterRegistrationBean.setInitParameters(initParams);
+        return filterRegistrationBean;
     }
 }
